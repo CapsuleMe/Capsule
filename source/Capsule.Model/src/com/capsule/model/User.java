9a3a844d756fbd4af198b8cbcd49ff226fc52bde
Chao Zhou <e-bread@qq.com>
2012-10-11 11:35:34 +0800

    添加了Model工程
    新加User类，只包含了登陆用的信息

---
+++ source/Capsule.Model/src/com/capsule/model/User.java
@@ -0,0 +1,27 @@
+package com.capsule.model;
+
+public class User {
+
+	private String id;
+	private String number;
+	private String password;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
