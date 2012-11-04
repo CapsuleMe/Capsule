469b2b2e31d3dc5fc4bc7c10b5608666909443b3
Sunxc <sunxianchao@gmail.com>
2012-11-04 16:04:27 +0800

    相关资源文件提交

--- source/Capsule/gen/com/capsule/android/R.java
+++ source/Capsule/gen/com/capsule/android/R.java
@@ -13,6 +13,13 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int login_btn=0x7f020002;
+        public static final int login_btn_press=0x7f020003;
+        public static final int login_btn_selector=0x7f020004;
+        public static final int register_btn=0x7f020005;
+        public static final int register_btn_press=0x7f020006;
+        public static final int register_btn_selector=0x7f020007;
+        public static final int welcome_bg=0x7f020008;
     }
     public static final class id {
         public static final int buttonForgetPassword=0x7f070005;
@@ -20,9 +27,12 @@ public final class R {
         public static final int buttonRegist=0x7f070006;
         public static final int editTextNumber=0x7f070001;
         public static final int editTextPassword=0x7f070003;
-        public static final int menu_settings=0x7f070007;
+        public static final int loginBtn=0x7f070009;
+        public static final int menu_settings=0x7f07000a;
+        public static final int registerBtn=0x7f070008;
         public static final int textViewNumber=0x7f070000;
         public static final int textViewPassword=0x7f070002;
+        public static final int welcomeLayout=0x7f070007;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
@@ -35,12 +45,13 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
-        public static final int input_number=0x7f040009;
-        public static final int input_password=0x7f040007;
-        public static final int login=0x7f040005;
+        public static final int input_number=0x7f04000a;
+        public static final int input_password=0x7f040008;
+        public static final int login=0x7f040006;
         public static final int menu_settings=0x7f040002;
-        public static final int number=0x7f040008;
-        public static final int password=0x7f040006;
+        public static final int number=0x7f040009;
+        public static final int password=0x7f040007;
+        public static final int register=0x7f040005;
         public static final int title_activity_login=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
