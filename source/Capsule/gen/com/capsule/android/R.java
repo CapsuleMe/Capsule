9975dc6e0535c5bf0afeff530d2d0cf96061bde5
Sunxc <sunxianchao@gmail.com>
2012-11-05 10:34:03 +0800

    增加一些字符串和颜色的配置

--- source/Capsule/gen/com/capsule/android/R.java
+++ source/Capsule/gen/com/capsule/android/R.java
@@ -10,6 +10,11 @@ package com.capsule.android;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int grey=0x7f040002;
+        public static final int white=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
@@ -19,43 +24,50 @@ public final class R {
         public static final int register_btn=0x7f020005;
         public static final int register_btn_press=0x7f020006;
         public static final int register_btn_selector=0x7f020007;
-        public static final int welcome_bg=0x7f020008;
+        public static final int round_edittext=0x7f020008;
+        public static final int welcome_bg=0x7f020009;
     }
     public static final class id {
-        public static final int buttonForgetPassword=0x7f070005;
-        public static final int buttonLogin=0x7f070004;
-        public static final int buttonRegist=0x7f070006;
-        public static final int editTextNumber=0x7f070001;
-        public static final int editTextPassword=0x7f070003;
-        public static final int loginBtn=0x7f070009;
-        public static final int menu_settings=0x7f07000a;
-        public static final int registerBtn=0x7f070008;
-        public static final int textViewNumber=0x7f070000;
-        public static final int textViewPassword=0x7f070002;
-        public static final int welcomeLayout=0x7f070007;
+        public static final int editTextNumber=0x7f080000;
+        public static final int editTextPassword=0x7f080001;
+        public static final int editTextUsername=0x7f080006;
+        public static final int loginBtn=0x7f080003;
+        public static final int menu_settings=0x7f080009;
+        public static final int registerBtn=0x7f080005;
+        public static final int textAgreement=0x7f080007;
+        public static final int textAgreement2=0x7f080008;
+        public static final int textForgetPassword=0x7f080002;
+        public static final int welcomeLayout=0x7f080004;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_register=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_login=0x7f060000;
-        public static final int activity_main=0x7f060001;
+        public static final int activity_login=0x7f070000;
+        public static final int activity_main=0x7f070001;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int input_number=0x7f04000a;
-        public static final int input_password=0x7f040008;
-        public static final int login=0x7f040006;
-        public static final int menu_settings=0x7f040002;
-        public static final int number=0x7f040009;
-        public static final int password=0x7f040007;
-        public static final int register=0x7f040005;
-        public static final int title_activity_login=0x7f040004;
-        public static final int title_activity_main=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int forget_password=0x7f05000c;
+        public static final int hello_world=0x7f050001;
+        public static final int input_number=0x7f05000b;
+        public static final int input_password=0x7f050009;
+        public static final int login=0x7f050007;
+        public static final int menu_settings=0x7f050002;
+        public static final int number=0x7f05000a;
+        public static final int password=0x7f050008;
+        public static final int register=0x7f050006;
+        public static final int register_agreement=0x7f05000d;
+        public static final int register_agreement2=0x7f05000e;
+        public static final int register_put_username=0x7f05000f;
+        public static final int title_activity_login=0x7f050004;
+        public static final int title_activity_main=0x7f050003;
+        public static final int title_activity_register=0x7f050005;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
+        public static final int txt=0x7f060001;
     }
 }
