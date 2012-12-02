d5c085d177f30ef48d985782583105b9ad2ccca8
Sunxc <sunxianchao@gmail.com>
2012-12-02 15:01:41 +0800

    被观察者对象存储位置信息

---
+++ source/Capsule/src/com/capsule/android/global/Status.java
@@ -0,0 +1,48 @@
+package com.capsule.android.global;
+
+import java.util.Observable;
+
+import android.location.Location;
+import android.util.Log;
+
+/**Singleton instance for saving properties. 
+ * @author chao_zhou
+ * @version 1.0.0.0
+ * */
+public class Status extends Observable{
+
+	private static Status currentStatus = null;
+	
+	public static Status getCurrentStatus(){
+		if(currentStatus == null){
+			currentStatus = new Status();
+		}
+		return currentStatus;
+	}
+	
+	private Location lastLocation = null;
+	private Location location = null;
+	
+	public Location getLastLocation() {
+		return lastLocation;
+	}
+
+	protected void setLastLocation(Location lastLocation) {
+		this.lastLocation = lastLocation;
+	}
+	
+	public Location getLocation(){
+		return location;
+	}
+
+	public void setLocation(Location l){
+		setLastLocation(location);
+		location = l;
+		
+		Log.d("capsule","location changed");
+		this.setChanged();
+		this.notifyObservers();
+	}
+
+	
+}
