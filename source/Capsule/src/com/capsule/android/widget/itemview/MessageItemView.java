59e7ef80f71f55811e0c505674fc0e5cae00b285
chao-zhou <eebread@gmail.com>
2012-12-11 14:26:42 +0800

    构建 Message Activity

---
+++ source/Capsule/src/com/capsule/android/widget/itemview/MessageItemView.java
@@ -0,0 +1,58 @@
+package com.capsule.android.widget.itemview;
+
+import greendroid.widget.AsyncImageView;
+import greendroid.widget.item.Item;
+import greendroid.widget.itemview.ItemView;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.capsule.android.R;
+import com.capsule.android.widget.item.MessageItem;
+
+public class MessageItemView extends RelativeLayout implements ItemView{
+
+	private AsyncImageView mHeadView = null;
+	private TextView mNameView = null;
+	private TextView mContentView = null;
+	private TextView mTimeView = null;
+	
+	public MessageItemView(Context context) {
+		this(context, null);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+	public MessageItemView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MessageItemView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public void prepareItemView() {
+		// TODO Auto-generated method stub
+		mHeadView = (AsyncImageView)findViewById(R.id.message_head);
+		mNameView = (TextView)findViewById(R.id.message_name);
+		mContentView = (TextView)findViewById(R.id.message_content);
+		mTimeView = (TextView)findViewById(R.id.message_time);
+		
+		mHeadView.setDefaultImageResource(R.drawable.ic_launcher);
+	}
+
+	public void setObject(Item item) {
+		// TODO Auto-generated method stub
+		final MessageItem mitem = (MessageItem)item;
+		mNameView.setText(mitem.msg.getUser().getName());
+		mContentView.setText(mitem.msg.getContent());
+		mTimeView.setText("10分钟前");
+		
+		
+	}
+
+}
