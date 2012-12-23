14b4174f71d760733e52aa932f0abd948e5aa28e
Sunxc <sunxianchao@gmail.com>
2012-12-23 20:58:45 +0800

    从设置界面到找TA 显示找ta方式列表

---
+++ source/Capsule/src/com/capsule/android/FindWaysActivity.java
@@ -0,0 +1,12 @@
+package com.capsule.android;
+
+import android.os.Bundle;
+
+
+public class FindWaysActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_ways);
+    }
+}
