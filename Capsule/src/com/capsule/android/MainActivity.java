b354785b36edd140bbebf2fd2cf0bd6033ef00f9
chao-zhou <eebread@gmail.com>
2013-07-16 16:13:04 +0800

    新写主页逻辑，实现自动登陆和导航

--- Capsule/src/com/capsule/android/MainActivity.java
+++ Capsule/src/com/capsule/android/MainActivity.java
@@ -1,8 +1,13 @@
 package com.capsule.android;
 
+import com.capsule.android.rest.RestFactory;
+import com.capsule.common.CommonUtil;
 import com.capsule.common.Navigator;
+import com.capsule.model.User;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 
@@ -15,14 +20,45 @@ public class MainActivity extends BaseActivity{
         setContentView(R.layout.activity_main);
         View layout=findViewById(R.id.welcomeLayout);
         layout.getBackground().setAlpha(180);
+        
+        User user = application.cache.getUser().getUser();
+		if(user == null){
+			goRegister(null);
+			return;
+		}
+		
+		CommonUtil.showLoadingDialog(MainActivity.this, "正在登录，请稍等片刻...");
+		new LoginTask().execute(user); 
     }
 
-    
-    public void goRegister(View v){
+	public void goRegister(View v){
         myNavigator.switchTo(Navigator.RegistActivitySEQ);
     }
     
     public void goLogin(View v){
         myNavigator.switchTo(Navigator.LoginActivitySEQ);
+    }
+    
+    public void goMain(){
+    	myNavigator.switchTo(Navigator.BottomTabActivitySEQ);
+    }
+    
+    private class LoginTask extends AsyncTask<User, Void, Integer> {
+
+		@Override
+		protected Integer doInBackground(User... users) {
+			User user = users[0];
+	    	return RestFactory.getUserClient().login(user.getId(), user.getPassword());
+		}
+    	
+		@Override
+		protected void onPostExecute(Integer val) {
+			// TODO Auto-generated method stub		
+			CommonUtil.closeLodingDialog();
+			Log.i("value",String.valueOf(val));
+			if(val == 0){
+				goMain();
+			}
+		}
     }
 }
